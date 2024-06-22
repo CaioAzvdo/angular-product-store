0902956bc337c613146d9e05d06457c0c8d3c919
CaioAzvdo <caioazevedodourado8@gmail.com>
2024-06-22 01:30:57 -0300

    Adiciona router-outlet ao AppComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {ListComponent} from "./features/list/list.component";
+// import * as path from "node:path";
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  component: ListComponent
+
+}
+
+];
