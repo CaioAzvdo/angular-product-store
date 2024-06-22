927e0cb6c3ad6c1c4a3a8c89aa6261d710c57031
CaioAzvdo <caioazevedodourado8@gmail.com>
2024-06-22 00:54:27 -0300

    Header components modificados

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatIcon, MatIconButton],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
