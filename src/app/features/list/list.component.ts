1e355c20397bd574d0952e4908b451b683e3d1f9
CaioAzvdo <caioazevedodourado8@gmail.com>
2024-06-27 17:32:35 -0300

    Criação do JSON server e utilização dos dados no list-component.html

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    JsonPipe
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  products: any[] = [];
+  httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.httpClient.get<any>('/api/products').subscribe((products: any) => {
+      this.products = products;
+    });
+  }
 
 }
